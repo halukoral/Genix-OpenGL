d17326c5b249ec53b5daacb00508f2e94443d262
Haluk <halukoral0@gmail.com>
2020-12-27 21:49:55 +0300

    Init Commit

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+int main()
+{
+	return 0;
+}
