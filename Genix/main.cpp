d6599ad8f619f69a85c97f2e25f478d574f22f6d
Haluk Oral <67242728+halukoral@users.noreply.github.com>
2024-01-05 21:05:00 +0300

    Windows initialized

--- Genix/main.cpp
+++ Genix/main.cpp
@@ -18,6 +18,52 @@ int main()
 	// Setup GLFW window properties
 	// OpenGL version
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	// Core profile = No backwards compatibility
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
+	GLFWwindow* MainWindow = glfwCreateWindow(WIDTH, HEIGHT, "Genix", nullptr, nullptr);
+	if (MainWindow == nullptr)
+	{
+		std::cout << "GLFW window creation failed!" << std::endl;
+		glfwTerminate();
+		return 1;
+	}
+
+	// Get buffer size information
+	int BufferWidth, BufferHeight;
+	glfwGetFramebufferSize(MainWindow, &BufferWidth, &BufferHeight);
+
+	// Set context for GLEW to use
+	glfwMakeContextCurrent(MainWindow);
+
+	// Allow modern extension features
+	glewExperimental = GL_TRUE;
+
+	if (glewInit() != GLEW_OK)
+	{
+		std::cout << "GLEW initialization failed!" << std::endl;
+		glfwDestroyWindow(MainWindow);
+		glfwTerminate();
+		return 1;
+	}
+
+	// Setup viewport size
+	glViewport(0, 0, BufferWidth, BufferHeight);
+
+	// Loop until window closed
+	while (!glfwWindowShouldClose(MainWindow))
+	{
+		// Handle input
+		glfwPollEvents();
+
+		// Clear window
+		glClearColor(1.f, 0.f, 0.f, 1.f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(MainWindow);
+	}
+	
 	return 0;
 }
