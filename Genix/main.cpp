9f56d744d194025f17fad6ae6a6e64e8e6e9ead9
Haluk Oral <67242728+halukoral@users.noreply.github.com>
2024-01-25 10:43:05 +0300

    Refactoring

--- Genix/main.cpp
+++ Genix/main.cpp
@@ -7,6 +7,7 @@
 
 #include "Shader.h"
 #include "Camera.h"
+
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
@@ -17,30 +18,32 @@ void ProcessInput(GLFWwindow *Window);
 constexpr GLint WIDTH = 1920;
 constexpr GLint HEIGHT = 1080;
 
-// camera
+// Camera
 Camera Camera(glm::vec3(0.0f, 0.0f, 3.0f));
 float LastX = WIDTH / 2.0f;
 float LastY = HEIGHT / 2.0f;
 bool FirstMouse = true;
 
-// timing
-float DeltaTime = 0.0f;	// time between current frame and last frame
+// Timing
+float DeltaTime = 0.0f;	// Current - Last
 float LastFrame = 0.0f;
 
 int main()
-{	
-	// Init GLFW
+{
+	// --------------------------------Initialization Phase---------------------------------------
+	// -------------------------------------------------------------------------------------------
+
 	if (!glfwInit())
 	{
 		std::cout << "GLFW initialization failed!" << std::endl;
 		glfwTerminate();
 		return 1;
 	}
-
-	// Setup GLFW window properties
+	
 	// OpenGL version
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
 	// Core profile = No backwards compatibility
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
@@ -61,8 +64,8 @@ int main()
 	glfwMakeContextCurrent(MainWindow);
 	glfwSetCursorPosCallback(MainWindow, MouseCallback);
 	glfwSetScrollCallback(MainWindow, ScrollCallback);
-
-	// tell GLFW to capture our mouse
+	
+	// Tell GLFW to capture our mouse
 	glfwSetInputMode(MainWindow, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
 	// Allow modern extension features
@@ -79,15 +82,18 @@ int main()
 	// Setup viewport size
 	glViewport(0, 0, BufferWidth, BufferHeight);
 
-	// configure global opengl state
-	// -----------------------------
+	// Configure global opengl state
 	glEnable(GL_DEPTH_TEST);
 	
-	// build and compile our shader program
-	// ------------------------------------
-	Shader Shader("Shaders/shader.vert", "Shaders/shader.frag");
+	// --------------------------------End Of Initialization Phase--------------------------------
+	// -------------------------------------------------------------------------------------------
 
-	// set up vertex data (and buffer(s)) and configure vertex attributes
+
+	// Build and compile our shader program
+	// ------------------------------------
+	const Shader Shader("Shaders/shader.vert", "Shaders/shader.frag");
+
+	// Set up vertex data (and buffer(s)) and configure vertex attributes
 	// ------------------------------------------------------------------
 	constexpr GLfloat Vertices[] = {
 		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
@@ -133,8 +139,8 @@ int main()
 		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
 	};
 	
-	// world space positions of our cubes
-	glm::vec3 cubePositions[] = {
+	// World space positions of our cubes
+	constexpr glm::vec3 CubePositions[] = {
 		glm::vec3( 0.0f,  0.0f,  0.0f),
 		glm::vec3( 2.0f,  5.0f, -15.0f),
 		glm::vec3(-1.5f, -2.2f, -2.5f),
@@ -147,58 +153,59 @@ int main()
 		glm::vec3(-1.3f,  1.0f, -1.5f)
 	};
 
+	/* Create variables to hold the VBO and the VAO identifiers */
 	GLuint VAO, VBO; //EBO;
+
 	glGenVertexArrays(1, &VAO);
-	// bind the Vertex Array Object first,
-	// then bind and set vertex buffer(s),
-	// and then configure vertex attributes(s).
 	glBindVertexArray(VAO);
 
-	glGenBuffers(1, &VBO);
-	//glGenBuffers(1, &EBO);
+	/* ------------------------------------ Initialise VBO ------------------------------------ */
+	/* ---------------------------------------------------------------------------------------- */
 
+	/* Create a new VBO and use the VBO to store the id */
+	glGenBuffers(1, &VBO);
+
+	/* Make the new VBO active */
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+	/* Upload vertex data to the video device */
 	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
-	
-	// position attribute
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+
+	// Position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), nullptr);
 	glEnableVertexAttribArray(0);
 	
-	// texture coord attribute
+	// Texture coord attribute
 	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
 
-	// Note that this is allowed, the call to glVertexAttribPointer registered VBO
-	// as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+	/* Make the new VBO active. */
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	// Remember: do NOT unbind the EBO while a VAO is active as the bound element buffer object IS stored in the VAO; keep the EBO bound.
 
-	// You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
-	// VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
-	glBindVertexArray(0);
-
+	
 	// uncomment this call to draw in wireframe polygons.
 	// glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
-	// load and create a texture 
-	// -------------------------
+	/* ------------------------------- Load and create a texture ------------------------------- */
+	/* ---------------------------------------------------------------------------------------- */
 
-	// texture 1
+	// Texture 1
 	unsigned int Texture1;
 	glGenTextures(1, &Texture1);
-	glBindTexture(GL_TEXTURE_2D, Texture1); // all upcoming GL_TEXTURE_2D operations now have effect on this texture object
+	glBindTexture(GL_TEXTURE_2D, Texture1);
+	// All upcoming GL_TEXTURE_2D operations now have effect on this texture object
 
-	// set the texture wrapping parameters
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
+	// Set the texture wrapping parameters
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
-	// set texture filtering parameters
+	// Set texture filtering parameters
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-	// load image, create texture and generate mipmaps
+	// Load image, create texture and generate mipmaps
 	int Width, Height, NrChannels;
-	// replace it with your own image path.
+	// Replace it with your own image path.
 	unsigned char *Data = stbi_load("Textures/container.jpg", &Width, &Height, &NrChannels, 0);
 	if (Data)
 	{
@@ -212,20 +219,21 @@ int main()
 	stbi_image_free(Data);
 
 
-	// texture 2
+	// Texture 2
 	unsigned int Texture2;
 	glGenTextures(1, &Texture2);
-	glBindTexture(GL_TEXTURE_2D, Texture2); // all upcoming GL_TEXTURE_2D operations now have effect on this texture object
+	glBindTexture(GL_TEXTURE_2D, Texture2);
+	// All upcoming GL_TEXTURE_2D operations now have effect on this texture object
 
-	// set the texture wrapping parameters
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
+	// Set the texture wrapping parameters
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
-	// set texture filtering parameters
+	// Set texture filtering parameters
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-	// load image, create texture and generate mipmaps
+	// Load image, create texture and generate mipmaps
 	stbi_set_flip_vertically_on_load(true);  
 	Data = stbi_load("Textures/awesomeface.png", &Width, &Height, &NrChannels, 0);
 	if (Data)
@@ -240,55 +248,61 @@ int main()
 	stbi_image_free(Data);
 	stbi_set_flip_vertically_on_load(false);  
 
-	// tell opengl for each sampler to which texture unit it belongs to (only has to be done once)
-	// -------------------------------------------------------------------------------------------
-	Shader.Use(); // don't forget to activate/use the shader before setting uniforms!
+	
+	/* ----------------------------------------- Shader --------------------------------------- */
+	/* ---------------------------------------------------------------------------------------- */
+
+	/* Set shader program as being actively used */
+	Shader.Use();
+
+	// don't forget to activate/use the shader before setting uniforms!
 	// either set it manually like so:
 	glUniform1i(glGetUniformLocation(Shader.ID, "texture1"), 0);
 	// or set it via the texture class
 	Shader.SetInt("texture2", 1);
+
+	/* ---------------------------------------------------------------------------------------- */
+	/* ---------------------------------------------------------------------------------------- */
+
 	
 	// Loop until window closed
 	while (!glfwWindowShouldClose(MainWindow))
 	{
-		// per-frame time logic
-		// --------------------
+		// Delta-time
 		const float CurrentFrame = static_cast<float>(glfwGetTime());
 		DeltaTime = CurrentFrame - LastFrame;
 		LastFrame = CurrentFrame;
 
-		// input
-		// -----
 		ProcessInput(MainWindow);
 		
 		// Clear window
 		glClearColor(0.2f, 0.3f, 0.3f, 1.f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // also clear the depth buffer now!
 
-		// bind textures on corresponding texture units
+		// Bind textures on corresponding texture units
 		glActiveTexture(GL_TEXTURE0);
 		glBindTexture(GL_TEXTURE_2D, Texture1);
 		glActiveTexture(GL_TEXTURE1);
 		glBindTexture(GL_TEXTURE_2D, Texture2);
 		
-		// activate shader
+		// Activate shader
 		Shader.Use();
 
-		// pass projection matrix to shader (note that in this case it could change every frame)
+		// Pass projection matrix to shader (note that in this case it could change every frame)
 		glm::mat4 Projection = glm::perspective(glm::radians(Camera.Zoom), (float)WIDTH / (float)HEIGHT, 0.1f, 100.0f);
 		Shader.SetMat4("projection", Projection);
 
-		// camera/view transformation
+		// Camera/view transformation
 		glm::mat4 View = Camera.GetViewMatrix();
 		Shader.SetMat4("view", View);
 
-		// render boxes
+		// Render boxes
 		glBindVertexArray(VAO);
 		for (unsigned int i = 0; i < 10; i++)
 		{
-			// calculate the model matrix for each object and pass it to shader before drawing
+			// Calculate the model matrix for each object and pass it to shader before drawing
 			glm::mat4 Model = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
-			Model = glm::translate(Model, cubePositions[i]);
+			Model = glm::translate(Model, CubePositions[i]);
 			const float Angle = 20.0f * i;
 			Model = glm::rotate(Model, glm::radians(Angle), glm::vec3(1.0f, 0.3f, 0.5f));
 			Shader.SetMat4("model", Model);
@@ -296,25 +310,27 @@ int main()
 			glDrawArrays(GL_TRIANGLES, 0, 36);
 		}
 		
-		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+		// Glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
 		// -------------------------------------------------------------------------------
 		glfwSwapBuffers(MainWindow);
 		glfwPollEvents();
 	}
 
-	// optional: de-allocate all resources once they've outlived their purpose:
+	// Optional: de-allocate all resources once they've outlived their purpose:
 	// ------------------------------------------------------------------------
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
 	//glDeleteBuffers(1, &EBO);
 
-	// glfw: terminate, clearing all previously allocated GLFW resources.
+	// Glfw: terminate, clearing all previously allocated GLFW resources.
 	// ------------------------------------------------------------------
 	glfwTerminate();
 	
 	return 0;
 }
 
+// GlFW: whenever the mouse moves, this callback is called
+// -------------------------------------------------------
 void MouseCallback(GLFWwindow* Window, double Xpos, double Ypos)
 {
 	const float X = static_cast<float>(Xpos);
@@ -336,12 +352,16 @@ void MouseCallback(GLFWwindow* Window, double Xpos, double Ypos)
 	Camera.ProcessMouseMovement(Xoffset, Yoffset);	
 }
 
+// GlFW: whenever the mouse scroll wheel scrolls, this callback is called
+// ----------------------------------------------------------------------
 void ScrollCallback(GLFWwindow* Window, double Xoffset, double Yoffset)
 {
 	Camera.ProcessMouseScroll(static_cast<float>(Yoffset));
 
 }
 
+// Process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
 void ProcessInput(GLFWwindow *Window)
 {
 	if (glfwGetKey(Window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
