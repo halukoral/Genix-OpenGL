96d386dde942995c6c6db67379f30dfcf9bb0129
Haluk Oral <67242728+halukoral@users.noreply.github.com>
2024-01-30 03:54:28 +0300

    Directional Light Implemented

--- main.cpp
+++ main.cpp
@@ -32,9 +32,6 @@ bool FirstMouse = true;
 float DeltaTime = 0.0f;	// Current - Last
 float LastFrame = 0.0f;
 
-// Lighting
-glm::vec3 LightPos(1.2f, 1.0f, 2.0f);
-
 int main()
 {
 	// --------------------------------Initialization Phase---------------------------------------
@@ -153,6 +150,20 @@ int main()
         -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f,  1.0f
     };
 
+	// positions all containers
+	glm::vec3 CubePositions[] = {
+		glm::vec3( 0.0f,  0.0f,  0.0f),
+		glm::vec3( 2.0f,  5.0f, -15.0f),
+		glm::vec3(-1.5f, -2.2f, -2.5f),
+		glm::vec3(-3.8f, -2.0f, -12.3f),
+		glm::vec3( 2.4f, -0.4f, -3.5f),
+		glm::vec3(-1.7f,  3.0f, -7.5f),
+		glm::vec3( 1.3f, -2.0f, -2.5f),
+		glm::vec3( 1.5f,  2.0f, -2.5f),
+		glm::vec3( 1.5f,  0.2f, -1.5f),
+		glm::vec3(-1.3f,  1.0f, -1.5f)
+	};
+	
 	/* --------------------------------- Initialise VAO & VBO --------------------------------- */
 	/* ---------------------------------------------------------------------------------------- */
 
@@ -233,7 +244,7 @@ int main()
 
 		// be sure to activate shader when setting uniforms/drawing objects
 		CubeMaterial.Use();
-		CubeMaterial.SetVec3("light.position", LightPos);
+		CubeMaterial.SetVec3("light.direction", -0.2f, -1.0f, -0.3f);
 		CubeMaterial.SetVec3("viewPos", Camera.Position);
 
 		// light properties
@@ -261,21 +272,19 @@ int main()
 		glActiveTexture(GL_TEXTURE1);
 		glBindTexture(GL_TEXTURE_2D, SpecularMap);
 		
-		// render the cube
+		// render containers
 		glBindVertexArray(CubeVAO);
-		glDrawArrays(GL_TRIANGLES, 0, 36);
+		for (unsigned int i = 0; i < 10; i++)
+		{
+			// calculate the model matrix for each object and pass it to shader before drawing
+			glm::mat4 model = glm::mat4(1.0f);
+			model = glm::translate(model, CubePositions[i]);
+			const float Angle = 20.0f * i;
+			model = glm::rotate(model, glm::radians(Angle), glm::vec3(1.0f, 0.3f, 0.5f));
+			CubeMaterial.SetMat4("model", model);
 
-		// also draw the lamp object
-		LightShader.Use();
-		LightShader.SetMat4("projection", Projection);
-		LightShader.SetMat4("view", View);
-		Model = glm::mat4(1.0f);
-		Model = glm::translate(Model, LightPos);
-		Model = glm::scale(Model, glm::vec3(0.2f)); // a smaller cube
-		LightShader.SetMat4("model", Model);
-
-		glBindVertexArray(LightVAO);
-		glDrawArrays(GL_TRIANGLES, 0, 36);
+			glDrawArrays(GL_TRIANGLES, 0, 36);
+		}
 
 		// ImGUI window creation
 		ImGui::Begin("My name is window, ImGUI window");
